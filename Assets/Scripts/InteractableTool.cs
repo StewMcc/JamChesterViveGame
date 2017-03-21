b32e6c09e78375b1ceb2f82ed8d59d19f0080d8a
Stewart L. McCready <stewmcc@gmail.com>
2017-03-21 16:21:33 +0000

    Update VRTK, Tools and Trophy class done.

---
+++ Assets/Scripts/InteractableTool.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class InteractableTool : VRTK_InteractableObject {
+
+	[Header("Tool Settings")]
+	[SerializeField]
+	Trophy.CleaningRule cleaningType = Trophy.CleaningRule.kNone;
+
+	[SerializeField]
+	float cleaningRate = 1.0f;
+
+	[SerializeField]
+	bool requiresUseToClean = false;
+
+	private Trophy currentTrophy = null;
+
+	protected override void OnEnable() {
+		base.OnEnable();
+		if (requiresUseToClean) {
+			GetComponent<VRTK_ControllerEvents>().AliasUseOn += UseItem;
+		}
+	}
+	protected override void OnDisable() {
+		base.OnDisable();
+		if (requiresUseToClean) {
+			GetComponent<VRTK_ControllerEvents>().AliasUseOn -= UseItem;
+		}
+	}
+
+	protected void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Trophy") {
+			currentTrophy = collider.gameObject.GetComponent<Trophy>();
+		}
+	}
+	
+	protected void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.tag == "Trophy") {
+			currentTrophy = null;
+		}
+	}
+
+	// clean the trophy if we have one, and the tool doesnt require use to clean the item.
+	protected override void Update() {
+		base.Update();
+		if(!requiresUseToClean && currentTrophy) {
+			currentTrophy.CleanTrophy(cleaningType, cleaningRate*Time.deltaTime);
+		}
+
+		// CleaningRule specific updates
+	}
+
+	// clean the trophy if we have one, when the tool is used.
+	protected virtual void UseItem(object sender, ControllerInteractionEventArgs e) {
+		Debug.Log("tool used");
+		if (currentTrophy) {
+			currentTrophy.CleanTrophy(cleaningType, cleaningRate);
+		}
+		// Clean the trophy specific on use.
+	}
+
+}
---
+++ Assets/Scripts/Trophy.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class Trophy : VRTK_InteractableObject {
+	const float kMaxPercentage_ = 1.0f;
+	public enum CleaningRule {
+		kNone, kBrushed, kWiped, kSponged, kSprayed
+	}
+
+	[Header("Trophy Settings")]
+	[SerializeField]
+	CleaningRule[] cleaningRules = new CleaningRule[4];
+
+	[SerializeField]
+	Renderer trophyBaseRenderer = null;
+	[SerializeField]
+	Renderer trophyTopRenderer = null;
+
+	[SerializeField]
+	float dirtyMultiplier = 1.0f;
+
+	private float percentageBrushed_ = kMaxPercentage_;
+	private float percentageWiped_ = kMaxPercentage_;
+	private float percentageSponged_ = kMaxPercentage_;
+	private float percentageSprayed_ = kMaxPercentage_;
+
+	protected override void Awake() {		
+		foreach (var rule in cleaningRules) {
+			switch (rule) {
+				case CleaningRule.kBrushed:
+					percentageBrushed_ = 0.0f;
+					break;
+				case CleaningRule.kWiped:
+					percentageWiped_ = 0.0f;
+					break;
+				case CleaningRule.kSponged:
+					percentageSponged_ = 0.0f;
+					break;
+				case CleaningRule.kSprayed:
+					percentageSprayed_ = 0.0f;
+					break;
+			}
+		}
+		SetEmissiveValue(1.0f-((percentageBrushed_ + percentageWiped_ + percentageSponged_ + percentageSprayed_) / (kMaxPercentage_ * 4.0f)));
+	}
+	
+	public bool IsClean() {
+		return (percentageBrushed_ >= 1) && (percentageWiped_ >= 1) && (percentageSponged_ >= 1) && (percentageSprayed_ >= 1);
+	}
+
+	public void CleanTrophy(CleaningRule cleaningType, float amount) {
+		
+		switch (cleaningType) {
+			case CleaningRule.kBrushed:
+				percentageBrushed_ += amount;
+				break;
+			case CleaningRule.kWiped:
+				percentageWiped_ += amount;
+				break;
+			case CleaningRule.kSponged:
+				percentageSponged_ += amount;
+				break;
+			case CleaningRule.kSprayed:
+				percentageSprayed_ += amount;
+				break;
+		}
+		ClampPercentages();
+		SetEmissiveValue(1.0f - ((percentageBrushed_ + percentageWiped_ + percentageSponged_ + percentageSprayed_) / (kMaxPercentage_ * 4.0f)));
+	}
+
+	private void ClampPercentages() {
+		if (percentageBrushed_ > kMaxPercentage_) {
+			percentageBrushed_ = kMaxPercentage_;
+		}
+		if (percentageWiped_ > kMaxPercentage_) {
+			percentageWiped_ = kMaxPercentage_;
+		}
+		if (percentageSponged_ > kMaxPercentage_) {
+			percentageSponged_ = kMaxPercentage_;
+		}
+		if (percentageSprayed_ > kMaxPercentage_) {
+			percentageSprayed_ = kMaxPercentage_;
+		}
+	}
+
+	private void SetEmissiveValue(float value) {
+		value *= dirtyMultiplier;
+		Debug.Log("EmissiveValue: " + value);
+		// change texture cleanliness :/		
+		Color baseColor = Color.white; //Replace this with whatever you want for your base color at emission level '1'
+		Color finalColor = baseColor * value;
+
+		trophyTopRenderer.material.SetColor("_EmissionColor", finalColor);
+		trophyBaseRenderer.material.SetColor("_EmissionColor", finalColor);
+	}
+
+
+}
